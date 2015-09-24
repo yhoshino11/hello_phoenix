8e2edb48e39b8051c6bb8f95005b8ab57cd83023
Yu Hoshino <yhoshino11@gmail.com>
2015-09-25 02:54:17 +0900

    Finish "Views"

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,10 @@
 defmodule HelloPhoenix.PageView do
   use HelloPhoenix.Web, :view
+
+  def message do
+    # $ iex -S mix
+    # iex(1)> Phoenix.View.render(HelloPhoenix.PageView, "test.html", %{})
+    # {:safe, [[["" | "This is the message: "] | "Hello from the view!"] | "\n"]}
+    "Hello from the view!"
+  end
 end
