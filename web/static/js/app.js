e26141c0c92d639c13379f0b610fc2a9742fcf97
Yu Hoshino <yhoshino11@gmail.com>
2015-09-26 04:02:14 +0900

    Finish "Channels"

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,26 @@ import "deps/phoenix_html/web/static/js/phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import {Socket} from "deps/phoenix/web/static/js/phoenix"
+let chatInput         = $("#chat-input")
+let messagesContainer = $("#messages")
+
+let socket = new Socket("/socket")
+socket.connect()
+
+let chan = socket.channel("rooms:lobby", {})
+
+chatInput.on("keypress", event => {
+  if (event.keyCode === 13) {
+    chan.push("new_msg", {body: chatInput.val()})
+    chatInput.val("")
+  }
+})
+
+chan.on("new_msg", payload => {
+  messagesContainer.append(`<br />[${Date()}] ${payload.body}`)
+})
+
+chan.join().receive("ok", chan => {
+  console.log("Welcome to Phoenix Chat!")
+})
